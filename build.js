045d28a9d51c433e1a50b0e67f00b506dd9aba1d
Sergej Sintschilin <seregpie@gmail.com>
2022-02-10 15:13:46 +0100

    +

---
+++ build.js
@@ -0,0 +1,1 @@
+let outDir = 'dist';
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import injectExternals from 'vite-plugin-inject-externals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    injectExternals({
+      modules: [
+        {
+          name: 'vue',
+          global: 'Vue',
+          path: 'https://unpkg.com/vue@3/dist/vue.global.prod.js'
+        },
+      ],
+    }),
+  ],
   base: '/VueBoom/',
   build: {
     outDir: 'docs',
